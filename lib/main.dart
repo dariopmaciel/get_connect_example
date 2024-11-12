b22df71dd4d83137c9e2649564c5b303a72104b7
dariopmaciel <dariodepaulamaciel@hotmail.com>
2024-11-12 09:37:57 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get_connect_example/home/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        useMaterial3: false,
+      ),
+      getPages: [
+        GetPage(name: '/', page: () => const HomePage()),
+      ],
+    );
+  }
+}
