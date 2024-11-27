3e55055c89175f12fe77305ee487f6db00c04ec1
dariopmaciel <dariopmaciel@hotmail.com>
2024-11-27 08:48:04 -0300

    final com exemplo

--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -4,22 +4,31 @@ import 'package:get_connect_example/models/user_model.dart';
 // class UserRepository extends GetConnect{
 
 class UserRepository {
-  final restCliente = GetConnect();
+  final restCliente = GetConnect(timeout: const Duration(milliseconds: 600));
+
+  UserRepository() {
+    restCliente.httpClient.baseUrl = 'http://10.0.0.107:8080';
+    // restCliente.httpClient.errorSafety = false;
+  }
 
   Future<List<UserModel>> findAll() async {
     //getter
-    final result = await restCliente.get('http://10.0.0.107:8080/users');
+    final result = await restCliente.get(
+      '/users',
+    );
 
     if (result.hasError) {
       throw Exception(
-          'Erro ao buscar usuário (${result.statusText})-(${result.statusCode})-(${result.status})');
+          'Erro ao buscar usuário ->(${result.statusText})->(${result.statusCode})->(${result.status})');
     }
-    return result.body.map<UserModel>((user) => UserModel.fromMap(user)).toList();
+
+    return result.body
+        .map<UserModel>((user) => UserModel.fromMap(user))
+        .toList();
   }
 
   Future<void> save(UserModel user) async {
-    final result =
-        await restCliente.post('http://10.0.0.107:8080/users', user.toMap());
+    final result = await restCliente.post('/users', user.toMap());
     if (result.hasError) {
       throw Exception(
           "Erro ao salvar usuario (${result.statusText})-(${result.statusCode})-(${result.status})");
@@ -27,8 +36,7 @@ class UserRepository {
   }
 
   Future<void> deleteUser(UserModel user) async {
-    final result =
-        await restCliente.delete('http://10.0.0.107:8080/users/${user.id}');
+    final result = await restCliente.delete('/users/${user.id}');
     if (result.hasError) {
       throw Exception(
           "Erro ao salvar usuario (${result.statusText})-(${result.statusCode})-(${result.status})");
@@ -36,8 +44,7 @@ class UserRepository {
   }
 
   Future<void> updateUser(UserModel user) async {
-    final result = await restCliente.put(
-        'http://10.0.0.107:8080/users/${user.id}', user.toMap());
+    final result = await restCliente.put('/users/${user.id}', user.toMap());
     if (result.hasError) {
       throw Exception(
           "Erro ao atualizar usuario (${result.statusText})-(${result.statusCode})-(${result.status})");
